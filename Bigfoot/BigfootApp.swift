3bcc6d381a3d11acfaabe7ddd85a8816e7b5e1c9
E-know <eknow25@gmail.com>
2025-04-03 22:36:43 +0900

    Initial Commit

---
+++ Bigfoot/BigfootApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigfootApp.swift
+//  Bigfoot
+//
+//  Created by Inho Choi on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BigfootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
